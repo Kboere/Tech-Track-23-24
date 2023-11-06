3616d7b5c921860b607fcce235be8dedc413eef6
Kboere <kevin.boere@hva.nl>
2023-11-06 11:30:51 +0100

    fullpage only css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,18 @@
 <svelte:head>
 	<link rel="stylesheet" href="style.css" />
-    <script src="custom.js"></script>
+	<script src="custom.js"></script>
 </svelte:head>
 
-<script>
-    import Bar from '../components/bar.svelte';
-</script>
+<main>
+	<article>
+		<section><Bar /></section>
+		<section>2</section>
+		<section>3</section>
+		<section>4</section>
+		<section>5</section>
+	</article>
+</main>
 
-<div class="container">
-    <!-- Each section is a div with the "section" class -->
-    <div class="section">
-     <h2>Section 1</h2>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-   </div>
-   <div class="section">
-    <Bar />
-     <h2>Section 2</h2>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-   </div>
-   <div class="section">
-     <h2>Section 3</h2>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-   </div>
-   <div class="section">
-     <h2>Section 4</h2>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-   </div>
-   <div class="section">
-     <h2>Section 5</h2>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-   </div>
- </div>
+<script>
+	import Bar from '../components/bar.svelte';
+</script>
