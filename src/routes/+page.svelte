d9799c1ff9bbb119e2dd5bf285c0a7f06dec52a4
Kboere <kevin.boere@hva.nl>
2023-11-06 09:37:01 +0100

    Start of project
    removed files that weren't needed and started on the horizontal slide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<link rel="stylesheet" href="style.css" />
+    <script src="custom.js"></script>
+</svelte:head>
+
+<script>
+    import Bar from '../components/bar.svelte';
+</script>
+
+<div class="container">
+    <!-- Each section is a div with the "section" class -->
+    <div class="section">
+     <h2>Section 1</h2>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+   </div>
+   <div class="section">
+    <Bar />
+     <h2>Section 2</h2>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+   </div>
+   <div class="section">
+     <h2>Section 3</h2>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+   </div>
+   <div class="section">
+     <h2>Section 4</h2>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+   </div>
+   <div class="section">
+     <h2>Section 5</h2>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+   </div>
+ </div>
