c7cd75e14a8ef20541360ec9b34b5b50091303fb
Kboere <kevin.boere@hva.nl>
2023-11-09 18:02:13 +0100

    added (svg)train-parts for horizontal scrolling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,45 @@
 	<script src="custom.js"></script>
 </svelte:head>
 
+<div class="train">
+	<img src="../images/train.svg" alt="train of NS">
+</div>
+
 <main>
 	<article>
 		<section>
 			<ProvincesMap />
+			<div class="train-part">
+				<img class="trainstation right" src="../images/links-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="track" src="../images/track.svg" alt="traintrack" />
+			</div>
 		</section>
 		<section>
-			<Bar />
+			<div class="train-part">
+				<img class="trainstation left" src="../images/rechts-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="trainstation right" src="../images/links-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="track" src="../images/track.svg" alt="traintrack" />
+			</div>
 		</section>
-		<section>3</section>
-		<section>4</section>
-		<section>5</section>
+		<section>
+			<div class="train-part">
+				<img class="trainstation left" src="../images/rechts-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="trainstation right" src="../images/links-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="track" src="../images/track.svg" alt="traintrack" />
+			</div>
+		</section>
+		<section>
+			<div class="train-part">
+				<img class="trainstation left" src="../images/rechts-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="trainstation right" src="../images/links-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="track" src="../images/track.svg" alt="traintrack" />
+			</div>
+		</section>
+		<section>
+			<div class="train-part">
+				<img class="trainstation left" src="../images/rechts-trainstation2.svg" alt="trainstation of geldermalsen">
+				<img class="track" src="../images/track.svg" alt="traintrack" />
+			</div>
+		</section>
 	</article>
 </main>
