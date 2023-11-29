d31dec2a57af2b65f5854c3290694577d69b6d77
Kboere <kevin.boere@hva.nl>
2023-11-29 14:33:52 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,13 +39,14 @@
 		</section>
 		<section class="explain-slide">
 			<div class="center-box">
-				<span>1/2</span>
+				<span>1/3</span>
 				<h3 class="fade-in">A general idea of where trainstations in the Netherlands are based</h3>
 				<p class="fade-in typed">
 					The Netherlands is a country where travelling with public transport is really being supported. 
 					It has a few benefits for the environment but also for students. 
 					Because the trains run on electricity, they emit nothing and public transport is made free during the week for students so they can go to school. 
-					<br><br>So I also use public transport and asked myself where the largest train stations would be.
+					<br><br>
+					So I also use public transport and asked myself where the largest train stations would be.
 					That's why you see in the next slide, a map of the largest trainstations in the Netherlands with some fun facts. Scroll to see!
 				</p>
 			</div>
@@ -83,7 +84,7 @@
 		</section>
 		<section class="explain-slide">
 			<div class="center-box">
-				<span>2/2</span>
+				<span>2/3</span>
 				<h3 class="fade-in">Now let's ses some data of my route</h3>
 				<p class="fade-in typed">
 					Every morning, I take the train from my house to school. 
@@ -122,11 +123,25 @@
 				<img class="track" src="../images/track.svg" alt="traintrack" />
 			</div>
 		</section>
-		<section>
+		<section class="explain-slide">
+			<div class="center-box">
+				<span>3/3</span>
+				<h3 class="fade-in">Now let's ses some data of my route</h3>
+				<p class="fade-in typed">
+					Every morning, I take the train from my house to school. 
+					Whilest scrolling through this project you will show you some fun facts of my (almost) every day little adventure to school.
+					Get to now my route!  
+				</p>
+			</div>
 			<div class="train-part">
 				<img
 					class="trainstation left"
 					src="../images/rechts-trainstation2.svg"
+					alt="trainstation of geldermalsen"
+				/>
+				<img
+					class="trainstation right"
+					src="../images/links-trainstation2.svg"
 					alt="trainstation of geldermalsen"
 				/>
 				<img class="track" src="../images/track.svg" alt="traintrack" />
